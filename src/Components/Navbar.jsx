51c3c36c2379e54c92aea6e3b1b00516cf04ac6d
Syed Muhammad Dawood <syedmuhammaddawood635@gmail.com>
2024-08-06 00:27:40 +0500

    changes done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       <div className="flex flex-shrink-0 items-center">
         <h1 className=" text-2xl">Syed M Dawood</h1>
       </div>
-      <div className="m-8 flex items-center justify-center gap-2 text-2xl">
+      <div className="m-8 flex items-center justify-center gap-2 text-2xl mr-2">
         <a
           href="https://www.linkedin.com/in/syed-muhammad-dawood/"
           target="_blank"
